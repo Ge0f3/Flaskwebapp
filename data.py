aa9b55cb7ef88b6b48271314b22cc72bfc43bc12
Geoffrey <geo.geofe@gmail.com>
2017-04-28 02:43:58 -0500

    created initial layout with three tabs

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles=[
+        {
+            'id':'1',
+            'title':'Article One ',
+            'body': "Lorem ipsum dolor sit amet, tota complectitur vis in. Sint nonumes civibus in quo, an est elitr philosophia. Vel et vitae indoctum suavitate. Ius velit debet imperdiet ad, lorem luptatum vix ex. ",
+            'author':'Geoffrey',
+            'created-date':'04/27/2017'
+        },
+        {
+            'id': '2',
+            'title': 'Article Two ',
+            'body': "Lorem ipsum dolor sit amet, tota complectitur vis in. Sint nonumes civibus in quo, an est elitr philosophia. Vel et vitae indoctum suavitate. Ius velit debet imperdiet ad, lorem luptatum vix ex. ",
+            'author': 'Geoffrey',
+            'created-date': '04/27/2017'
+        },
+        {
+            'id': '3',
+            'title': 'Article Three ',
+            'body': "Lorem ipsum dolor sit amet, tota complectitur vis in. Sint nonumes civibus in quo, an est elitr philosophia. Vel et vitae indoctum suavitate. Ius velit debet imperdiet ad, lorem luptatum vix ex. ",
+            'author': 'Geoffrey',
+            'created-date': '04/27/2017'
+        }
+    ]
+    return articles
